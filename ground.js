618db1e2970bc4720df9fc3d3c4bc159a7db0a1f
vinmysterx <94560455+vinmysterx@users.noreply.github.com>
2022-01-05 19:01:17 +0000

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,31 @@
+class Ground 
+{
+  constructor(x, y, w, h) 
+  {
+    let options = {
+      isStatic:true
+    };
+    
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    this.x = x;
+    this.y = y;
+
+    World.add(world, this.body);
+  }
+
+  display() {
+    var groundPos = this.body.position;
+    push();
+    translate(groundPos.x, groundPos.y);
+    rectMode(CENTER);
+    stroke(255);
+    fill(127);
+    rect(0, 0, this.w, this.h);
+    pop();
+  }
+  
+}
+
+
